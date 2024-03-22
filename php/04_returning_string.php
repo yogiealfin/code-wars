cf6b9536e23ef11fe74502477157406ccc039c13
yogiealfin <yogiealfin23@gmail.com>
2024-03-22 10:34:27 +0700

    cleaner code for 04_returning_string (php)

--- php/04_returning_string.php
+++ php/04_returning_string.php
@@ -5,7 +5,7 @@ The goal of this challenge is to Make a function that will return a greeting sta
 function greet($name)
 {
   // Good Luck (like you need it)
-  return "Hello, ${name} how are you doing today?";
+  return "Hello, $name how are you doing today?";
 }
 
 echo greet("Yogie");
