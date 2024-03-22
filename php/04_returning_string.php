9bcf9f8005114457beff374f490ea89e9be7bae7
yogiealfin <yogiealfin23@gmail.com>
2024-03-22 10:29:27 +0700

    04_returning_string (php)

---
+++ php/04_returning_string.php
@@ -0,0 +1,11 @@
+<?php
+/*
+The goal of this challenge is to Make a function that will return a greeting statement that uses an input.
+*/
+function greet($name)
+{
+  // Good Luck (like you need it)
+  return "Hello, ${name} how are you doing today?";
+}
+
+echo greet("Yogie");
