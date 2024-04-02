f3a8abdca9f9720ac8761685927d7f455521596b
yogiealfin <yogiealfin23@gmail.com>
2024-04-02 15:53:38 +0700

    07_sentence_smash (js)

---
+++ js/07_sentence_smash.js
@@ -0,0 +1,7 @@
+// The goal of this challenge is to build a function that combine an array of string into one sentence
+
+function smash(words) {
+  return words.join(" ");
+}
+
+console.log(smash(["hello", "world"]));
